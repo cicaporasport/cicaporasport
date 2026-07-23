852c8252a4ff934b97e55a062a510add61d3fc6d
cicaporasport <cicapora.sc@gmail.com>
2026-07-24 01:43:32 +0700

    perbaikan tampilan dan logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,7 @@ export default function RootLayout({
   return (
     <html lang="id">
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#000000" />
       </head>
